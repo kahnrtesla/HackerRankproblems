f2807b3016c6f56261c6482d7baeb77acad5d1bd
kahnrtesla <60194876+kahnrtesla@users.noreply.github.com>
2021-04-15 18:38:10 -0400

    Add files via upload

---
+++ scrap.py
@@ -0,0 +1,11 @@
+wrd = "Weird"
+nwrd = "Not Weird"
+
+natnum = int(input("give me a natural number"))
+dirp = natnum/2
+wornum = str(dirp)
+
+if wornum[-1] == '5':
+    print("its odd")
+else:
+    print("its even")
